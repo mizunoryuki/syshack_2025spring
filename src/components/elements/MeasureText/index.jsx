9396165790118f61c60d0f979f9e9e270e22a2ea
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-03-25 18:36:34 +0900

    measureページの作成

---
+++ src/components/elements/MeasureText/index.jsx
@@ -0,0 +1,23 @@
+import "./index.css";
+export default function MeasureText({ type = "", title, content = "" }) {
+    if (type === "volume") {
+        return (
+            <div className="measureText-container">
+                <p className="title">{title}</p>
+                <p className="content volume">{content}</p>
+            </div>
+        );
+    } else if (type === "timer") {
+        const min = content / 60;
+        const sec = String(content % 60).padStart(2, "0");
+
+        return (
+            <div className="measureText-container">
+                <p className="title">{title}</p>
+                <p className="content">
+                    {min}:{sec}
+                </p>
+            </div>
+        );
+    }
+}
--- src/pages/Measure/index.jsx
+++ src/pages/Measure/index.jsx
@@ -1,9 +1,12 @@
+import Meter from "../../components/elements/Meter";
 import Title from "../../components/elements/Title";
+import "./index.css";
 
 export default function Measure() {
     return (
-        <div>
+        <div className="measure-container">
             <Title title={"音量測定"} text={"結果を表示"} />
+            <Meter />
         </div>
     );
 }
