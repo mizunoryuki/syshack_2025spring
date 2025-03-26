a23bf150733b08b7fc4d94add584984723e872de
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-03-26 14:37:23 +0900

    resultGraphの作成

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,12 +1,16 @@
+import ResultGraph from "../../components/elements/ResultGraph";
 import ResultText from "../../components/elements/ResultText";
 import Title from "../../components/elements/Title";
+import Button from "../../components/elements/Button";
 import "./index.css";
 export default function Result() {
     return (
         <div className="result-container">
             <Title title={"結果閲覧"} text={"保存"} />
             <div className="result-box">
-                <div className="result-left"></div>
+                <div className="result-left">
+                    <ResultGraph className="result-left" />
+                </div>
                 <div className="result-right">
                     <ResultText type="volume" title={"最高音量"} content={50} />
                     <ResultText
@@ -20,6 +24,11 @@ export default function Result() {
                         content={50}
                     />
                 </div>
+            </div>
+            <div className="result-buttons-container">
+                <Button text={"もう一度測定"} />
+                <Button text={"ホーム"} />
+                <Button text={"アンケート"} />
             </div>
         </div>
     );
