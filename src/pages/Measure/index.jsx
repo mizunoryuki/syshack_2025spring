f4acd13ec6d35fbeff5183aef78467d3f98ee9ab
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-03-28 17:26:29 +0900

    resultに飛べるようにした

--- src/pages/Measure/index.jsx
+++ src/pages/Measure/index.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import Meter from "../../components/elements/Meter";
 import Title from "../../components/elements/Title";
 import "./index.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Measure() {
+    const navigate = useNavigate();
     const [isdone, setIsdone] = useState(false);
     return (
         <div
@@ -12,7 +14,11 @@ export default function Measure() {
                 height: "60%",
             }}
         >
-            <Title title={"音量測定"} text={isdone ? "結果を表示" : ""} />
+            <Title
+                title={"音量測定"}
+                text={isdone ? "結果を表示" : ""}
+                clickFunction={() => navigate("/result")}
+            />
             <Meter isdone={isdone} setIsdone={setIsdone} />
         </div>
     );
