5978999972beb9cc27dd3ab825ec4ffc6bf4f3e5
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-03-29 01:03:12 +0900

    音量の取得を実装

--- src/components/elements/Meter/index.jsx
+++ src/components/elements/Meter/index.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
 import Button from "../Button";
 import MeasureText from "../MeasureText";
 import "./index.css";
 import useCountTime from "../../../hooks/timerhook";
 export default function Meter({ isdone, setIsdone }) {
-    const { count, isRunning, startTimer, finishMeasure } = useCountTime();
+    const { count, isRunning, avgdB, startTimer, finishMeasure, showdB } =
+        useCountTime();
 
     const handleFinishTimer = () => {
         finishMeasure();
+        localStorage.setItem(
+            "data",
+            JSON.stringify({ time: count, volume: avgdB })
+        );
+        console.log("save to localstorage");
         setIsdone(true);
     };
 
@@ -18,12 +23,12 @@ export default function Meter({ isdone, setIsdone }) {
                 <div
                     className="volume-meter"
                     style={{
-                        width: "20%",
+                        width: `${Math.min(showdB, 100)}%`,
                     }}
                 ></div>
             </div>
             <div className="measure-text-container">
-                <MeasureText type="volume" title={"音量"} content={50} />
+                <MeasureText type="volume" title={"音量"} content={showdB} />
                 {/* 音量をcontentで指定して表示 */}
                 <MeasureText type="timer" title={"時間"} content={count} />
                 {/* 時間をcontentで指定して表示(1秒ごと) */}
@@ -31,6 +36,7 @@ export default function Meter({ isdone, setIsdone }) {
             <div className="measure-button">
                 {!isdone ? (
                     <Button
+                        logotype={isRunning ? "stop" : "start"}
                         text={isRunning ? "計測停止" : "計測開始"}
                         Clickfunction={() =>
                             isRunning ? handleFinishTimer() : startTimer()
--- src/hooks/timerhook.js
+++ src/hooks/timerhook.js
@@ -1,31 +1,54 @@
 import { useState, useEffect } from "react";
+import useRecordVolume from "./recordVolume";
 
 export default function useCountTime() {
-    const [count, setCount] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
-    const [isdone, setIsdone] = useState(false);
+    const [count, setCount] = useState(0); //カウント
+    const [isRunning, setIsRunning] = useState(false); //カウントが動いているか
+    const [isSampling, setIsSampling] = useState(false); //サンプリングが行われたか
+    const [avgdB, setAvgdB] = useState(0); //算出した平均を出す
+    const { avgdBArray, showdB, startRecording, stopRecording } =
+        useRecordVolume();
 
+    //useEffect内で秒数のカウントとデータとして使う音量の計算を行う
     useEffect(() => {
         let timer;
         if (isRunning) {
             timer = setInterval(() => {
-                setCount((prev) => prev + 1);
+                setCount((prev) => prev + 1); //カウント
+
+                //平均音量を算出
+                const sum = avgdBArray.current.reduce(
+                    (acc, cur) => acc + cur,
+                    0
+                );
+                // setAvgdB(Math.floor(sum / avgdBArray.lenght))
+                console.log(Math.floor(sum / avgdBArray.current.length));
             }, 1000);
         } else {
             clearInterval(timer);
         }
-
         return () => clearInterval(timer);
-    }, [isRunning]);
+    }, [avgdBArray, isRunning]);
 
     const startTimer = () => {
+        setCount(0);
         setIsRunning(true);
+        startRecording();
     };
 
     const finishMeasure = () => {
         setIsRunning(false);
-        setIsdone(true);
+        setIsSampling(true);
+        stopRecording();
     };
 
-    return { count, isRunning, isdone, startTimer, finishMeasure };
+    return {
+        count,
+        isRunning,
+        isSampling,
+        avgdB,
+        showdB,
+        startTimer,
+        finishMeasure,
+    };
 }
